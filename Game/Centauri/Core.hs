7fbcd50a0f0cbc38680e0b72fc493041f494de92
Duncan Burke <duncankburke@gmail.com>
2014-10-25 18:35:46 +1100

    Split out graphical

---
+++ Game/Centauri/Core.hs
@@ -0,0 +1,14 @@
+{- |
+   module      : Game.Centauri.Core
+   copyright   : (c) Duncan Burke
+   license     : MPL
+   maintaner   : Duncan Burke <duncankburke@gmail.cmo>
+-}
+module Game.Centauri.Core
+       ( GameState,
+         GameMap,
+         Planar,
+         Hex ) where
+
+import Game.Centauri.Core.State
+import Game.Centauri.Core.Map
---
+++ Game/Centauri/Core/State.hs
@@ -0,0 +1,11 @@
+{- |
+   module      : Game.Centauri.Core.State
+   copyright   : (c) Duncan Burke
+   license     : MPL
+   maintaner   : Duncan Burke <duncankburke@gmail.cmo>
+-}
+module Game.Centauri.Core.State
+       ( GameState ) where
+
+data GameState = GameState {
+                           }
