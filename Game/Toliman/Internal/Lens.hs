dd64ce865e4f5d1997a84ca5b3be42d82f3f3916
Duncan Burke <duncankburke@gmail.com>
2014-12-17 19:49:18 +1100

    Lens: added combinators for State and Reader IORef

--- Game/Toliman/Internal/Lens.hs
+++ Game/Toliman/Internal/Lens.hs
@@ -1,14 +1,47 @@
 
 module Game.Toliman.Internal.Lens (
   module Control.Lens,
-  use ) where
+  use, (^=.),
+  assign, (.=),
+  use_ref, (^*.),
+  assign_ref, (.*)
+  ) where
 
 import Monad.State
 import Monad.Reader
+import Data.IORef
 import Control.Monad.Lift.IO
 
-import Control.Lens hiding (use)
+import Control.Lens hiding (use, assign, (.=))
 
 
 use :: MonadState s m => Getting a s a -> m a
 use l = gets $ view l
+
+(^=.) :: MonadState s m => Getting a s a -> m a
+(^=.) = use
+
+assign :: MonadState s m => ASetter s s a b -> b -> m ()
+assign l x = do
+  s <- get
+  put $ s & l .~ x
+
+(.=) :: MonadState s m => ASetter s s a b -> b -> m ()
+(.=) = assign
+
+use_ref :: (MonadReader (IORef s) m, MonadIO m) => Getting a s a -> m a
+use_ref l = do
+  s <- ask >>= liftIO . readIORef
+  return $ s ^. l
+
+(^*.) :: (MonadReader (IORef s) m, MonadIO m) => Getting a s a -> m a
+(^*.) = use_ref
+
+assign_ref :: (MonadReader (IORef s) m, MonadIO m) => ASetter s s a b -> b -> m ()
+assign_ref l x = do
+  ref <- ask
+  s <- liftIO $ readIORef ref
+  liftIO $ writeIORef ref $ s & l .~ x
+
+(.*) :: (MonadReader (IORef s) m, MonadIO m) => ASetter s s a b -> b -> m ()
+(.*) = assign_ref
